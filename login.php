421f1028858c97acf4d7e5589100003322a23f15
tinko0o <tinhneko2002@gmail.com>
2024-03-15 09:22:40 +0700

    revert ve commit so 2

--- login.php
+++ login.php
@@ -18,12 +18,6 @@
                         <label for="username">ten dang nhap:</label>
                         <input type="text" class="form-control" id="username" name="username" required>
                     </div>
-
-                    <div class="form-group">
-                        <label for="password">mat khau:</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block">Dang nhap</button>
             </div>
         </div>
     </div>
