95abc0054e9eb4a161952d945a62cd42f02c2556
yenvy1709 <113962362+yenvy1709@users.noreply.github.com>
2024-03-15 08:54:43 +0700

    them label mat khau, textbox: txtmatkhau

--- login.php
+++ login.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-md-6">
-                <form action="result.php" method="POST" class="mt-4">
+                < action="result.php" method="POST" class="mt-4">
                     <div class="form-group">
                         <label for="username">UserName:</label>
                         <input type="text" class="form-control" id="username" name="username" required>
                     </div>
-                </form>
+
+                    <div class="form-group">
+                        <label for="password">Mật khẩu:</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
